ea38de9c43d8dff0a2a711ff1971ec4caf3ee399
svnelif <elifatsvn2003@gmail.com>
2024-07-29 15:08:16 +0300

    Project Updated.

---
+++ FirebaseApp/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  FirebaseApp
+//
+//  Created by Elif Ataseven  on 29.07.2024.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
